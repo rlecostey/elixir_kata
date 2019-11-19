8302c3b96d81aadc7e65f6205a6b0d3d799b924a
Rémy Lecostey <remy.lecostey@gmail.com>
2019-11-19 11:57:37 +0100

    lib/elixir_kata.ex

--- lib/elixir_kata.ex
+++ lib/elixir_kata.ex
@@ -12,7 +12,36 @@ defmodule ElixirKata do
       :world
 
   """
-  def hello do
-    :world
+  def reverse_string_1(string) do
+    String.reverse(string)
+  end
+
+  def reverse_string_2(string) do
+    string
+    |> String.codepoints
+    |> Enum.reduce([], fn(el, acc) ->
+      List.insert_at(acc, 0, el)
+    end)
+    |> Enum.join
+  end
+
+  def reverse_string_3(string) do
+    string
+    |> String.codepoints
+    |> Enum.reverse(string)
+    |> Enum.join
+  end
+
+  def reverse_string_4(string) do
+    string
+    |> String.codepoints
+    |> Enum.reduce("", fn(el, acc) ->
+      el <> acc
+    end)
+  end
+
+  def reverse_string_5(string) do
+    string
+    |>
   end
 end
